5efb50d4d49cf195dd0361efb346cac5e2ca77f6
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-02-26 14:57:12 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
 
-# serialized = open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read
-serialized = open('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007').read
-cocktail = JSON.parse(serialized)
 
-
-# Ingredients
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "orange")
-Ingredient.create(name: "whisky")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "rhum")
-Ingredient.create(name: "citron")
-Ingredient.create(name: "citron vert")
-Ingredient.create(name: "jus de citron")
-Ingredient.create(name: "romarin")
-Ingredient.create(name: "thym")
-Ingredient.create(name: "ginger beer")
-Ingredient.create(name: "tonic")
-Ingredient.create(name: "blanc d'oeuf")
-Ingredient.create(name: "bitter")
-Ingredient.create(name: "sucre")
-
-# Cocktails
-Cocktail.create(name: "Gin Tonic")
-Cocktail.create(name: "Whisky Sour")
-Cocktail.create(name: "Ti Punch")
-Cocktail.create(name: "Canyon Drive")
