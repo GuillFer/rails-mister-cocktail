23adb0e88bab77b7c36aa24ef79226fa11faf669
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-02-26 20:17:54 +0100

    additionnal css

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -19,10 +19,20 @@ class CocktailsController < ApplicationController
     redirect_to cocktail_path(@cocktail)
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to root_path
+  end
+
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
-    redirect_to root_path(@cocktail)
+    redirect_to root_path
   end
 
   private
