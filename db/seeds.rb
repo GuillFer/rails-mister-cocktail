8fa08d100487f8d33fd4fd850bd03e5722ac61f3
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-02-26 22:56:54 +0100

    sorted ingredients collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,22 @@
 require 'json'
 require 'open-uri'
 
-
+Ingredient.create(name: "eau gazeuse")
+Ingredient.create(name: "Cointreau")
+Ingredient.create(name: "cola")
+Ingredient.create(name: "cachaça")
+Ingredient.create(name: "vermouth")
+Ingredient.create(name: "Campari")
+Ingredient.create(name: "sirop de sucre de canne")
+Ingredient.create(name: "bourbon")
+Ingredient.create(name: "limonade")
+Ingredient.create(name: "vanille")
+Ingredient.create(name: "concombre")
+Ingredient.create(name: "champagne")
+Ingredient.create(name: "triple sec")
+Ingredient.create(name: "cognac")
+Ingredient.create(name: "ananas")
+Ingredient.create(name: "fruit de la passion")
+Ingredient.create(name: "anisette")
+Ingredient.create(name: "sirop d'orgeat")
+Ingredient.create(name: "menthe")
