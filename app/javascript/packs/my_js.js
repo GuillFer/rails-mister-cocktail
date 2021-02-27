a793f7d7b601ad839ee24ca646629e96b572a31c
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-02-27 13:35:14 +0100

    responsive home

---
+++ app/javascript/packs/my_js.js
@@ -0,0 +1,6 @@
+// const cards = document.querySelectorAll('a');
+// console.log(cards);
+// cards.forEach(element => {
+//   width = element.style.width;
+//   element.style.height = width;
+// });
